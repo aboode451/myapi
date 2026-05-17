555b15a6a774927e636e44282b5b25a8e4fd60d4
Abdallah Najm Abd <udemylearn707@gmail.com>
2026-05-17 09:47:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
